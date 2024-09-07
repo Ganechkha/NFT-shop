cd5a1fda336547daee2f95ec643d93089865f14e
Ganechkha <maksim15848@gmail.com>
2024-09-07 20:44:19 +0300

    Editing HTML and CSS

--- nft_shop/account/views.py
+++ nft_shop/account/views.py
@@ -1,6 +1,8 @@
 from django.http import HttpRequest, HttpResponse
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
 
 def dashboard(request: HttpRequest) -> HttpResponse:
-    return render(request, "account/dashboard.html")
+    return render(request, "account/dashboard.html",
+                  {"section": "dashboard"})
