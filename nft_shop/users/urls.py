8ee244567e66ca16992728b07120bc37986904bf
Ganechkha <maksim15848@gmail.com>
2024-09-26 11:36:40 +0300

    Add user list page with pagination

--- nft_shop/users/urls.py
+++ nft_shop/users/urls.py
@@ -3,6 +3,9 @@ from django.urls import path
 from . import views
 
 
+app_name = "users"
+
+
 urlpatterns = [
     path("", views.UserListView.as_view(),
          name="users_list"),
--- nft_shop/users/views.py
+++ nft_shop/users/views.py
@@ -12,7 +12,9 @@ User = get_user_model()
 @method_decorator(login_required, name="dispatch")
 class UserListView(ListView):
     model = User
+    queryset = User.objects.filter(is_active=True).order_by("-id")
     template_name = "users/list.html"
+    context_object_name = "users"
     paginate_by = 12
 
     def get_context_data(self, *, object_list=None, **kwargs):
